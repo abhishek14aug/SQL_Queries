7a2650a3873f71d17534bb339df78d736295e0ee
Abhishek Kumar Chaurasi <abhishekchaurasia14aug@gmail.com>
2022-12-13 23:46:29 +0530

    insert  rows into customers table

---
+++ insert_into_customers_table.sql
@@ -0,0 +1,6 @@
+insert into customers values(1,'Abhishek',20,'UP',200000.00);
+insert into customers  values(2,'Abhay',18,'UP',300000.00);
+insert into customers values(3,'Mayand',21,'Delhi',200000.00);
+insert into customers  values(4,'Chitra',25,'Odisha',40000.00);
+insert into customers values(5,'Neha',22,'Kashmir',100000.00);
+insert into customers  values(6,'Rahul',22,'Punjab',700000.00);
